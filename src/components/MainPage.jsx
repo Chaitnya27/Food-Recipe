304cc364fe9cb420a079fa6ec62383efdbd5dea2
Chaitnya27 <chaitnyakhedekar02@gmail.com>
2025-06-20 00:35:25 +0530

    layout

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -39,10 +39,21 @@ function MainPage() {
         <div className="flex flex-col items-center mt-7">
          <h1 className=' text-3xl font-bold' >FOOD RECIPE APP</h1><br />
          <div className=' flex '>
-         <input type="text" placeholder='Search Dish,eg: Veg,Cake etc' 
-         className=' border-x-sky-50 bg-gray-200 rounded-xl h-10 w-100  text-xl pl-2'
+         <input type="text" placeholder='Search Dish,eg: Veg,Cake etc ' 
+         className='border-amber-50 bg-gray-200 rounded-xl h-10  pl-2
+          w-full
+         
+          sm:w-[15rem] sm:text-sm
+           md:w-[25rem] md:text-lg
+            lg:w-[30rem] lg:text-xl'
           onChange={handleInput}/>
-         <button className=' border-2 ml-2 bg-orange-500 h-10 w-25 rounded-xl text-amber-50 px-4 text-xl cursor-pointer hover:bg-teal-300  '
+         <button className="border-2 ml-2 bg-orange-500 h-10  rounded-xl
+          text-amber-50 px-4  cursor-pointer hover:bg-teal-300 
+          w-full
+          sm:w-[5rem] sm:text-sm sm:text-center
+          md:w-[6rem] md:text-base
+          lg:w-[8rem] lg:text-xl
+          "
           onClick={myfun}>Search</button>
          </div>
          <h4 className='font-bold mt-10'>{msg}</h4>
--- src/components/MealInfo.jsx
+++ src/components/MealInfo.jsx
@@ -21,20 +21,49 @@ function MealInfo() {
         <div className=" bg-blue-200">
             {
                 !info? "":
-                <div className="flex ">
-                <img className="h-100 w-100
+                <div className="flex sm:justify-center items-center
+
+                sm:flex-col 
+                md:flex-row 
+                lg:flex-row
+                
+                
+                
+                
+                ">
+                <img className="
                   p-4 mt-10 ml-5 rounded-4xl 
-                     hover:cursor-pointer "   
+                     hover:cursor-pointer 
+            
+                    sm:h-[25rem] sm:w-[30rem]  
+                    md:h-[23rem] md:w-[20rem]
+                    lg:h-[27rem] lg:w-[25rem]
+                     "   
                 src={info.strMealThumb} />
-                <div className=" flex  flex-col">
-                <h1 className="mt-15 ml-5 font-bold text-3xl">Recipe Details</h1>
-                <button className=" bg-red-500 text-amber-50 
-                ml-5 mt-5  h-10 w-35 text-shadow-2xs  font-bold 
-                rounded-xl  shadow-2xl ">
+                <div className=" flex  flex-col 
+                sm:justify-center items-center
+                ">
+                <h1 className="mt-10 ml-5 font-bold 
+                sm:text-lg  
+                md:text-2xl
+                lg:text-3xl
+                ">Recipe Details</h1>
+                <button className=" bg-red-500 text-amber-50 w-full
+                ml-5 mt-5  h-10 text-shadow-2xs  font-bold 
+                rounded-xl  shadow-2xl
+                sm:w-[10rem] sm:text-xs
+                md:w-[15rem] md:text-base
+                lg:w-[20rem] lg:text-lg 
+                ">
                     {info.strMeal}
                     </button>
                 <h3 className=" text-black font-bold ml-5 mt-5">Intruction's</h3>
-                <p className=" mt-5 ml-5 font-medium align-middle">{info.strInstructions}</p>
+                <p className=" mt-5 ml-5 font-medium 
+                sm:text-sm 
+                md:text-base md:font-sans
+                lg:text-lg  lg:font-serif
+                
+                ">{info.strInstructions}</p>
                 </div>
                 </div>
 
